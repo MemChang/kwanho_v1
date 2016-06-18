e78cf6ebc1e5d26251d4d6da83cb0c85d7659967
leekwanho <rhksgh7492@naver.com>
2016-06-18 22:49:38 +0900

    add anim
    add anim

---
+++ MemChang2/Assets/DestroyEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour {
+
+    public AudioClip audioClip;
+    public GameObject explosion;
+    
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnDestroy()
+    {
+        GameObject go = (GameObject) Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(go, 1.0f);        
+        
+        AudioSource.PlayClipAtPoint(audioClip, this.transform.position);
+    }
+    
+}
